e989912284665d319e2404605926205184b5c4f2
SomeThing05 <ombg1004@gmail.com>
2025-08-06 18:00:02 +0530

    completed hero section with other half section

--- The Hive/assets/js/main.js
+++ The Hive/assets/js/main.js
@@ -1,7 +1,18 @@
 //for working of hameburger manue
-const hameburger=document.getElementById("hameburger");
-const navLinks=document.getElementById("nav-links");
+const hameburger = document.getElementById('hameburger');
+const navLinks = document.getElementById('nav-links');
 
-hameburger.addEventListener('click',()=>{
-    navLinks.classList.toggle('active');
+hameburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+//for cahnging color of main navbar when user scrolls
+
+window.addEventListener('scroll', function () {
+  const navbar = document.getElementById('main-navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
 });
